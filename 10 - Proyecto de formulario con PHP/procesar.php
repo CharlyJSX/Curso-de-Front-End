6ab2a8e97c130bc0fadc66ce1758e40df81492c9
Charly Mancía <77645310+CharlyJSX@users.noreply.github.com>
2022-08-23 16:09:22 -0600

    Add files via upload

---
+++ 10 - Proyecto de formulario con PHP/procesar.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombre = $POST['nombre'];
+$apellido = $POST['apellido'];
+$edad = $POST['edad'];
+$pw = $POST['pw'];
+
+echo $nombre "<br>";
+echo $apellido "<br>";
+echo $edad "<br>";
+echo $pw;
+
+?>
